a50fbaf685824c0a65a5d8b04cac1d72503bbed2
ParticleChurch <jj6403729@gmail.com>
2022-04-01 19:29:22 -0700

    auto update shell

---
+++ src/auto_update_worker.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <QtCore>
+#include "http.hpp"
+
+#include <Windows.h>
+
+#define VERSION "0.0.1"
+
+class AutoUpdateWorker : public QThread
+{
+    Q_OBJECT;
+
+public:
+    using QThread::QThread;
+
+    void run() override
+    {
+        auto response = HTTP::get(
+            "injector/version",
+            {
+                { "my_version", VERSION },
+            }
+        );
+
+        if (response.error)
+        {
+            //MessageBox(NULL, "Hello", "Goodbye", MB_ICONERROR);
+        }
+
+        if (response.status_code == 200)
+        {
+            HTTP::JSON::json body = HTTP::JSON::json::parse(response.text);
+            if (body.contains("session_id") && body.contains("user_id"))
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+        else
+        {
+            
+        }
+    }
+};
